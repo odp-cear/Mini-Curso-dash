45a961359a1b9e88eb165483ead00f131be699a6
Samuel Batista dos Santos <samuelxntj@gmail.com>
2026-02-26 16:11:50 -0300

    Adicionando Codigo das Aulas

---
+++ Desafios/Desafio_Aula2.py
@@ -0,0 +1,12 @@
+#NESSE ARQUIVO VOCÊ IRÁ CONSTRUIR O CÓDIGO DO DESAFIO 2
+
+#Desafio 2: 
+#- Criar um diretório novo na sua máquina;
+#- Abrir o diretório no VSCode
+#- Clonar o repositório do minicurso do GitHub no diretório vazio;
+#- Criar um arquivo "app.py" ;
+#- Construir uma rotina python que mostre em linhas subsequentes:
+#- Seu nome
+#- Seu curso
+#- O período que você está.
+#- Executar o código via terminal.
---
+++ Desafios/Desafio_Aula3.py
@@ -0,0 +1,8 @@
+#NESSE ARQUIVO VOCÊ IRÁ CONSTRUIR SEU CÓDIGO DO DESAFIO 3
+
+#Desafio 3: Utilizando Jupyter Notebook
+#- Construir um código com título "Hello Dash" utilizando a biblioteca dash
+#- Criar um subtítulo para a aplicação Dash, que deve conter:
+#- > "Meu primeiro código utilizando Dash"
+#- Rodar o Código no terminal
+#- Alterar o título principal do código para ("Minicurso Python Dash)
